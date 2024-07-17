17eafd525aee12adab73ea13d5eb37e0e13ea42a
Ezequiel Ângelo <ezequiel_angelo_bbu@hotmail.com>
2024-07-17 16:55:15 -0400

    operadores

---
+++ curso-java-basico/src/Operadores.java
@@ -0,0 +1,44 @@
+public class Operadores {
+    
+
+    public static void main(String[] args) {
+        
+
+        //Concatenação
+        String nomeCompleto = "LINGUAGEM "+"JAVA";
+
+        System.out.println(nomeCompleto);
+
+        String concatenacao = "?";
+
+        concatenacao = 1 + 1 +"1";
+
+        System.out.println(concatenacao);
+
+        concatenacao = 1+"1"+1+"1";
+
+        System.out.println(concatenacao);
+
+        //Operadores Unários
+
+        int numero = 5;
+
+        System.out.println(-numero);
+
+        System.out.println(numero);
+
+
+        //Incrementos
+
+        int numeroC = 5;
+
+        numeroC = numeroC + 1;
+
+        System.out.println(numero);
+
+
+
+
+        
+    }
+}
