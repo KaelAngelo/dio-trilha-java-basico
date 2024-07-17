de9b6777d8e248dcf60fb396d7533c9cdabdd059
Ezequiel Ângelo <ezequiel_angelo_bbu@hotmail.com>
2024-07-17 15:49:10 -0400

    variaveis e constantes

--- curso-java-basico/src/TiposVariaveis.java
+++ curso-java-basico/src/TiposVariaveis.java
@@ -1,5 +1,39 @@
+import javax.swing.JOptionPane;
+
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+       
+        double salarioMinimo = 2500;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+
+        //casting na variavel int para short
+        short numeroCurto2 = (short) numeroNormal;
+
+        //Declaração sem a palavra reserva final, aceita mudar o valor
+        int numero = 1;
+
+        numero = 2;
+
+        //Variáveis e constantes
+
+        System.out.println("Aceitou mudar o valor: "+numero);
+
+        //Constantes deverão ser escrita em CAIXA ALTA
+        final int DIA_MES = 30;
+
+        DIA_MES = 30;
+
+       
+
+
+
+
+      
+
+        
+
+      
     }
 }
