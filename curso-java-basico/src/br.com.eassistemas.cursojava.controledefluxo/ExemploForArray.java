0e4135fa7fd196a04ec3cded506acc0dd941fb31
Ezequiel Ângelo <ezequiel_angelo_bbu@hotmail.com>
2024-07-22 12:10:17 -0400

    Expressao Condifionla

---
+++ curso-java-basico/src/br.com.eassistemas.cursojava.controledefluxo/ExemploForArray.java
@@ -0,0 +1,12 @@
+public class ExemploForArray {
+    
+    public static void main(String[] args) {
+        String filhos [] = {"EZEQUIEL", "GABRIEL","DANIEL","ISABELA","JOÃO MIGUEL","PEDRO"};
+
+        for (String string : filhos) {
+            
+            System.out.println(string);
+            System.out.println(filhos[0]);
+        }
+    }
+}
